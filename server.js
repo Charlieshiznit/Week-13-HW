3327be7be1a399b9e7266c0497acd531dabdf631
Charles Aaron Lewis <caaronlewis@gmail.com>
2016-08-07 17:08:27 -0500

    more ish

---
+++ server.js
@@ -0,0 +1,29 @@
+// more dependencies 
+var express = require ('express');
+var bodyParser = require('body-Parser');
+var path = require('path');
+
+//now this should set up the Express app and it allow the server to work.
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// uses any static files required by the html files.
+app.use(express.static('app/public/'));
+
+// Sets up the Express app to handle data parsing 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+// ROUTER
+// The below points our server to "route" files within the directory.
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app); 
+
+
+// Starts the server 
+app.listen(PORT, function() {
+  console.log('App listening on PORT ' + PORT);
+});
